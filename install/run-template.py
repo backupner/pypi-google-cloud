dfbbc22af4b9f012290aa6ae700aae46eb627463
Alexander Ovchinnikov <alexander@entropia.us>
2020-04-16 05:03:05 +0300

    pypi-google-cloud-23 Automate `Proxy` service creation

--- install/run-template.py
+++ install/run-template.py
@@ -1,30 +1,43 @@
 from typing import Any, Dict, List
 
 
-# GENERATE_RANDOM_TOKEN_COMMAND = "head -c '500' '/dev/urandom'" \
-#                                 " | tr -dc 'a-zA-Z0-9'" \
-#                                 " | fold -w '33'" \
-#                                 " | head -n '1'"
-#
-# CREATE_TOKEN_STDIN_COMMAND_TEMPLATE = "gcloud secrets create " \
-#                                       "'{deployment}-token' " \
-#                                       "--data-file='-' " \
-#                                       "--labels='app={deployment}' " \
-#                                       "--replication-policy='automatic' " \
-#                                       "--project='{project}'"
-#
-# CREATE_TOKEN_COMMAND_TEMPLATE = ' | '.join([GENERATE_RANDOM_TOKEN_COMMAND, CREATE_TOKEN_STDIN_COMMAND_TEMPLATE])
-#
-# DELETE_TOKEN_COMMAND_TEMPLATE = "gcloud secrets delete " \
-#                                 "'{deployment}-token' " \
-#                                 "--project='{project}'"
-#
-# GRANT_ROLE_COMMAND_TEMPLATE = "gcloud secrets add-iam-policy-binding " \
-#                               "'{deployment}-token' " \
-#                               "--member='serviceAccount:{deployment}-proxy@{project}.iam.gserviceaccount.com' " \
-#                               "--role='roles/secretmanager.secretAccessor' " \
-#                               "--project='{project}'"
-INTERNAL_PYPI_GCS_PROXY_IMAGE_SUFFIX = 'pypi-gcs-proxy:latest'
+REGION = 'europe-west1'
+
+ENV_VARS_TEMPLATE = ','.join([
+    'STATIC_BUCKET_NAME={deployment}-{project}-static',
+    'PACKAGES_BUCKET_NAME={deployment}-{project}-packages',
+    'TOKEN_NAME=projects/{project}/secrets/{deployment}-token/versions/1',
+])
+
+DEPLOY_CONTAINER_ARGS_TEMPLATE = [
+    'run', 'deploy', '{deployment}-gcs-proxy',
+    '--image', '{internal_pypi_gcs_proxy_image}',
+    '--args', '',
+    '--command', '',
+    '--concurrency', '80',
+    '--cpu', '1',
+    '--max-instances', '10',
+    '--memory', '128Mi',
+    '--platform', 'managed',
+    '--port', '8080',
+    '--timeout', '5m',
+    '--set-env-vars', '{env_vars}',
+    '--update-labels', 'app={deployment}',
+    '--allow-unauthenticated',
+    '--service-account', '{deployment}-proxy@{project}.iam.gserviceaccount.com',
+    '--region', REGION,
+    '--project', '{project}',
+]
+
+DELETE_SERVICE_ARGS_TEMPLATE = [
+    'run', 'services', 'delete',
+    '{deployment}-gcs-proxy',
+    '--platform', 'managed',
+    '--region', REGION,
+    '--project', '{project}',
+]
+
+INTERNAL_PYPI_GCS_PROXY_IMAGE_TEMPLATE = 'eu.gcr.io/{project}/{deployment}-gcs-proxy:latest'
 PYPI_GCS_PROXY_IMAGE = 'backupner/pypi-gcs-proxy:latest'
 
 
@@ -34,11 +47,17 @@ def generate_config(context: Any) -> Dict[str, List]:
     Todo: create custom type providers (or wait for Google)"""
     deployment = context.env['deployment']
     project = context.env['project']
-    image_name_template = 'eu.gcr.io/{project}/{INTERNAL_PYPI_GCS_PROXY_IMAGE_SUFFIX}'
-    internal_pypi_gcs_proxy_image = image_name_template.format(project=project, INTERNAL_PYPI_GCS_PROXY_IMAGE_SUFFIX=INTERNAL_PYPI_GCS_PROXY_IMAGE_SUFFIX)
-    # create_token_command = CREATE_TOKEN_COMMAND_TEMPLATE.format(deployment=deployment, project=project)
-    # delete_token_command = DELETE_TOKEN_COMMAND_TEMPLATE.format(deployment=deployment, project=project)
-    # grant_role_command = GRANT_ROLE_COMMAND_TEMPLATE.format(deployment=deployment, project=project)
+    internal_pypi_gcs_proxy_image = INTERNAL_PYPI_GCS_PROXY_IMAGE_TEMPLATE.format(deployment=deployment, project=project)
+    deploy_container_args = DEPLOY_CONTAINER_ARGS_TEMPLATE
+    deploy_container_args[2] = deploy_container_args[2].format(deployment=deployment)
+    deploy_container_args[4] = internal_pypi_gcs_proxy_image
+    deploy_container_args[24] = ENV_VARS_TEMPLATE.format(deployment=deployment, project=project)
+    deploy_container_args[26] = deploy_container_args[26].format(deployment=deployment)
+    deploy_container_args[29] = deploy_container_args[29].format(deployment=deployment, project=project)
+    deploy_container_args[33] = project
+    delete_container_args = DELETE_SERVICE_ARGS_TEMPLATE
+    delete_container_args[3] = delete_container_args[3].format(deployment=deployment)
+    delete_container_args[9] = project
     resources = [
         {
             'name': 'download-{deployment}-proxy-container-image'.format(deployment=deployment),
@@ -74,72 +93,49 @@ def generate_config(context: Any) -> Dict[str, List]:
                 ],
             },
         },
-        # {
-        #     'name': 'deploy-{deployment}-proxy-run-container'.format(deployment=deployment),
-        #     'action': 'gcp-types/cloudbuild-v1:cloudbuild.projects.builds.create',
-        #     'metadata': {
-        #         'dependsOn': [
-        #             'download-{deployment}-proxy-container-image'.format(deployment=deployment),
-        #         ],
-        #         'runtimePolicy': ['CREATE'],
-        #     },
-        #     'properties': {
-        #         'steps': [
-        #             # {
-        #             #     'name': 'gcr.io/cloud-builders/gcloud',
-        #             #     'entrypoint': 'bash',
-        #             #     'args': [
-        #             #         '-c',
-        #             #         create_token_command,
-        #             #     ],
-        #             # },
-        #             # {
-        #             #     'name': 'gcr.io/cloud-builders/gcloud',
-        #             #     'entrypoint': 'bash',
-        #             #     'args': [
-        #             #         '-c',
-        #             #         grant_role_command,
-        #             #     ],
-        #             # },
-        #         ],
-        #         'timeout': '120s',
-        #         'options': {
-        #             'env': [
-        #                 'CLOUDSDK_CORE_DISABLE_PROMPTS=1',
-        #             ],
-        #         },
-        #         'tags': [deployment],
-        #     },
-        # },
-        # {
-        #     'name': 'delete-{deployment}-proxy-run-service'.format(deployment=deployment),
-        #     'action': 'gcp-types/cloudbuild-v1:cloudbuild.projects.builds.create',
-        #     'metadata': {
-        #         'runtimePolicy': ['DELETE'],
-        #     },
-        #     'properties': {
-        #         'steps': [
-        #             # {
-        #             #     'name': 'gcr.io/cloud-builders/gcloud',
-        #             #     'entrypoint': 'bash',
-        #             #     'args': [
-        #             #         '-c',
-        #             #         delete_token_command,
-        #             #     ],
-        #             #     'env': [
-        #             #         'CLOUDSDK_CORE_DISABLE_PROMPTS=1',
-        #             #     ],
-        #             # },
-        #         ],
-        #         'timeout': '120s',
-        #         'options': {
-        #             'env': [
-        #                 'CLOUDSDK_CORE_DISABLE_PROMPTS=1',
-        #             ],
-        #         },
-        #         'tags': [deployment],
-        #     },
-        # },
+        {
+            'name': 'deploy-{deployment}-proxy-run-container'.format(deployment=deployment),
+            'action': 'gcp-types/cloudbuild-v1:cloudbuild.projects.builds.create',
+            'metadata': {
+                'dependsOn': [
+                    'download-{deployment}-proxy-container-image'.format(deployment=deployment),
+                ],
+                'runtimePolicy': ['CREATE'],
+            },
+            'properties': {
+                'steps': [
+                    {
+                        'name': 'gcr.io/cloud-builders/gcloud',
+                        'args': deploy_container_args,
+                        'env': [
+                            'CLOUDSDK_CORE_DISABLE_PROMPTS=1',
+                        ],
+                    },
+                ],
+                'timeout': '120s',
+                'tags': [deployment],
+            },
+        },
+        {
+            'name': 'delete-{deployment}-proxy-run-service'.format(deployment=deployment),
+            'action': 'gcp-types/cloudbuild-v1:cloudbuild.projects.builds.create',
+            'metadata': {
+                'runtimePolicy': ['DELETE'],
+            },
+            'properties': {
+                'steps': [
+                    {
+                        'name': 'gcr.io/cloud-builders/gcloud',
+                        'args': delete_container_args,
+                        'env': [
+                            'CLOUDSDK_CORE_DISABLE_PROMPTS=1',
+                        ],
+                    },
+                ],
+                'timeout': '120s',
+                'tags': [deployment],
+            },
+        },
     ]
     return {
         'resources': resources,
